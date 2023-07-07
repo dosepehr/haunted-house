c5f135c41a5e095a8a4acaefdd3eab9b221ea2d7
dosepehr <dosepehr@gmail.com>
2023-07-07 19:27:07 +0330

    add roof

--- main.js
+++ main.js
@@ -35,6 +35,14 @@ const walls = new THREE.Mesh(
 );
 walls.position.y = 2.5 / 2;
 house.add(walls);
+// roof
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5, 1, 4),
+    new THREE.MeshStandardMaterial({ color: '#b35f45' })
+);
+roof.position.y = 2.5 + 0.5;
+roof.rotation.y = Math.PI * 0.25;
+house.add(roof);
 
 // lights
 

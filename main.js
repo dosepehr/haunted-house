f1f4d10d7c2bc848095663e1c7aa69a3d4355b49
dosepehr <dosepehr@gmail.com>
2023-07-07 22:51:00 +0330

    add door textures

--- main.js
+++ main.js
@@ -14,6 +14,21 @@ camera.position.set(4, 2, 5);
 scene.add(camera);
 
 /**
+ * textures
+ */
+// door textures
+const textureLoader = new THREE.TextureLoader();
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg');
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load(
+    '/textures/door/ambientOcclusion.jpg'
+);
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg');
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg');
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+
+/**
  * FOG
  */
 const fog = new THREE.Fog('#262837', 2, 15);
@@ -51,8 +66,22 @@ roof.rotation.y = Math.PI * 0.25;
 house.add(roof);
 // door
 const door = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 2),
-    new THREE.MeshStandardMaterial({ color: 'red' })
+    new THREE.PlaneGeometry(2.2, 2.2, 100, 100),
+    new THREE.MeshStandardMaterial({
+        map: doorColorTexture,
+        alphaMap: doorAlphaTexture,
+        transparent: true,
+        aoMap: doorAmbientOcclusionTexture,
+        displacementMap: doorHeightTexture,
+        displacementScale: 0.1,
+        normalMap: doorNormalTexture,
+        metalnessMap: doorMetalnessTexture,
+        roughnessMap: doorRoughnessTexture,
+    })
+);
+door.geometry.setAttribute(
+    'uv2',
+    new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array, 2)
 );
 door.position.y = 1;
 door.position.z = 2 + 0.01;

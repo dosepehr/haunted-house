49c3e641fcac4a23be8b0c4fbcb07d31ff6b4394
dosepehr <dosepehr@gmail.com>
2023-07-07 18:29:54 +0330

    add renderer

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
 
+const canvas = document.querySelector('.webgl');
+
 const sizes = {
     w: window.innerWidth,
     h: window.innerHeight,
@@ -9,4 +11,9 @@ const sizes = {
 const scene = new THREE.Scene();
 const camera = new THREE.Camera(75, sizes.w / sizes.h);
 scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.w, sizes.h);
+
+renderer.render(scene, camera);
 
